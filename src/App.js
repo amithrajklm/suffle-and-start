5f1be2f806c5f71b8c8beb2b880870272e652e3c
Amith Raj <amithrajklm@gmail.com>
2022-07-14 13:22:55 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [numbers, setNumbers] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  const colorClass = {
+    1: "color-three",
+    2: "color-two",
+    3: "color-one",
+    4: "color-two",
+    5: "color-one",
+    6: "color-four",
+    7: "color-four",
+    8: "color-three",
+    9: "color-one"
+  }
+  const handleShuffle = () => {
+    const shuffleNumber = numbers.sort( () => .5 - Math.random() )
+    setNumbers(JSON.parse(JSON.stringify(shuffleNumber)))
+  }
+  const handleSort = () => {
+    const sortNumber = numbers.sort();
+    setNumbers(JSON.parse(JSON.stringify(sortNumber)))
+  }
+
+  const renderCards = () => {
+    return (<div className='card-box'>
+      <div className='row'>
+        {numbers.map((item,index)=>{
+          if(index<3)
+          return <div key={index} className={`card ${colorClass[item]}`}>{item}</div>
+        })}
+      </div>
+      <div className='row'>
+      {numbers.map((item,index)=>{
+        if(index>=3 && index<6)
+          return <div key={index} className={`card ${colorClass[item]}`}>{item}</div>
+        })}
+      </div>
+      <div className='row'>
+      {numbers.map((item,index)=>{
+        if(index>=6 && index<9)
+          return <div key={index} className={`card ${colorClass[item]}`}>{item}</div>
+        })}
+      </div>
+    </div>)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <header className='app-header'>Suffle and Sort</header>
+      <div className='app-body'>
+        {renderCards()}
+        <div className='button-section'>
+          <button className='app-button' onClick={() => handleShuffle()}>SHUFFLE</button>
+          <button className='app-button' onClick={() => handleSort()}>SORT</button>
+        </div>
+      </div>
+      <footer className='app-footer'>Suffle and sort by Amith Raj</footer>
     </div>
   );
 }
